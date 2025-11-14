a77a0d83d97075ce842dbf85f8627487d0895d27
Danaheand <147558636+Danaheand@users.noreply.github.com>
2025-11-14 12:06:35 -0500

    cambio en el nombre de app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:Dfind/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Build our app and trigger a frame.N
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
